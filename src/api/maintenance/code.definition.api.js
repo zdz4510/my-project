dfa3cafd808e72de1254b1a895a4480b9c3678ff
huyoujuan <youjuan.hu@percent.cn>
2020-01-06 19:55:33 +0800

    通用代码定义

--- src/api/maintenance/code.definition.api.js
+++ src/api/maintenance/code.definition.api.js
@@ -25,3 +25,37 @@ export const findGeneralCodeHttp = data => {
     }
   );
 };
+
+/**
+ * 根据通用代码名（字段）从字段表模糊查询字段信息
+ *  url /mes/generalCodeService/findField
+ */
+export const findFieldHttp = data => {
+  return request.get(`${window.VUE_APP_URL}/mes/generalCodeService/findField`, {
+    params: data
+  });
+};
+
+/**
+ * 删除通用代码信息
+ *  url /mes/generalCodeService/deleteGeneralCode
+ */
+export const deleteGeneralCodeHttp = data => {
+  return request.delete(
+    `${window.VUE_APP_URL}/mes/generalCodeService/deleteGeneralCode`,
+    {
+      params: data
+    }
+  );
+};
+
+/**
+ * 新增或保存通用代码信息
+ *  url /mes/generalCodeService/saveGeneralCode
+ */
+export const saveGeneralCodeHttp = data => {
+  return request.post(
+    `${window.VUE_APP_URL}/mes/generalCodeService/saveGeneralCode`,
+    data
+  );
+};
