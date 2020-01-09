56e08e6413679881c3d19dd1028d4fc20179c99b
lvfang <15915364030@163.com>
2020-01-09 08:47:40 +0800

    更新了工单维护

--- src/views/work-order/pages/work-order.vue
+++ src/views/work-order/pages/work-order.vue
@@ -2,170 +2,187 @@
   <div class="workOrder">
     <div class="query">
       <div class="left">
-        <el-form
-          label-width="100px"
-          class="typeForm"
-        >
-          <el-form-item label="工单">
-            <el-input
-              placeholder="请输入工单"
-              v-model="workOrder"
-            ></el-input>
+        <el-form label-width="100px" class="typeForm">
+          <el-form-item label="工单:">
+            <el-input placeholder="请输入工单" v-model="shopOrder"></el-input>
           </el-form-item>
         </el-form>
       </div>
-      <div class="choiceBox"><i class="el-icon-document-copy"></i></div>
+      <div class="choiceBox">
+        <i class="el-icon-document-copy"></i>
+      </div>
       <div class="right">
-        <el-button size="small" type="primary">
-          查询
-        </el-button>
-        <el-button size="small" type="primary">
-          重置
-        </el-button>
+        <el-button size="small" type="primary" @click="getOrder">查询</el-button>
+        <el-button size="small" type="primary">重置</el-button>
       </div>
     </div>
     <div class="operate">
-      <el-button size="small" type="primary" >保存</el-button>
+      <el-button size="small" type="primary">保存</el-button>
       <el-button size="small" type="danger">删除</el-button>
     </div>
 
     <div class="showInfo">
-        <el-tabs type="border-card">
-            <el-tab-pane>
-                <span slot="label"><i class="el-icon-date"></i> 一般</span>
-                <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-                    <el-form-item label="类型：" prop="style">
-                        <el-select v-model="ruleForm.style" placeholder="生产/返工">
-                            <el-option label="生产" value="生产"></el-option>
-                            <el-option label="返工" value="返工"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="状态：" prop="state">
-                        <el-select v-model="ruleForm.state" placeholder="可下达/完成/关闭">
-                            <el-option label="可下达" value="可下达"></el-option>
-                            <el-option label="完成" value="完成"></el-option>
-                            <el-option label="关闭" value="关闭"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="计划物料:" prop="material" >
-                        <el-col :span="9" style="margin-right:7px;">
-                            <el-input 
-                                placeholder="请输入计划物料" v-model="ruleForm.material"
-                            ></el-input>
-                        </el-col>
-                        <div class="choiceBox"><i class="el-icon-document-copy"></i></div>
-                        <!--物料版本-->
-                        <div class="version">
-                            <el-form>
-                                <el-form-item label="版本：">
-                                    <el-col :span="12">
-                                        <el-input v-model="ruleForm.materialVer"></el-input>
-                                    </el-col>
-                                </el-form-item>
-                            </el-form>
-                        </div>
-                    </el-form-item>
-                    <el-form-item label="计划工艺路线:" label-width='150'>
-                        <el-col :span="9" style="margin-right:7px;">
-                            <el-input 
-                                placeholder="计划工艺路线" v-model="ruleForm.technologyLine"
-                            ></el-input>
-                        </el-col>
-                        <div class="choiceBox"><i class="el-icon-document-copy"></i></div>
-                        <!--计划工艺路线版本-->
-                        <div class="version">
-                            <el-form>
-                                <el-form-item label="版本：">
-                                    <el-col :span="12">
-                                        <el-input v-model="ruleForm.technologyLineVer"></el-input>
-                                    </el-col>
-                                </el-form-item>
-                            </el-form>
-                        </div>
-                    </el-form-item>
-                    <el-form-item label="生产数量:" prop="number">
-                        <el-col :span="9" style="margin-right:31px;">
-                            <el-input 
-                                placeholder="请输入生产数量" v-model="ruleForm.number"
-                            ></el-input>
-                        </el-col>
-                        <!--已下达数量-->
-                        <div>
-                            <el-form>
-                                <el-form-item label="已下达数量：">
-                                    <el-col :span="14">
-                                        <el-input v-model="ruleForm.issuedNum" :disabled="true"></el-input>
-                                    </el-col>
-                                </el-form-item>
-                            </el-form>
-                        </div>
-                    </el-form-item>
-                </el-form>
-            </el-tab-pane>
-            <el-tab-pane label="自定义字段">
+      <el-tabs type="border-card">
+        <el-tab-pane>
+          <span slot="label">
+            <i class="el-icon-date"></i> 一般
+          </span>
+          <el-form
+            :model="ruleForm"
+            :rules="rules"
+            ref="ruleForm"
+            label-width="100px"
+            class="demo-ruleForm"
+          >
+            <el-form-item label="类型：" prop="style">
+              <el-select v-model="ruleForm.style" placeholder="生产">
+                <el-option label="生产" value="PRODUCTION"></el-option>
+                <el-option label="返工" value="REWORK"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="状态：" prop="state">
+              <el-select v-model="ruleForm.state" placeholder="可下达/完成/关闭">
+                <el-option label="可下达" value="RELEASABLE"></el-option>
+                <el-option label="完成" value="DONE"></el-option>
+                <el-option label="关闭" value="CLOSE"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="计划物料:" prop="material">
+              <el-col :span="9" style="margin-right:7px;">
+                <el-input placeholder="请输入计划物料" v-model="ruleForm.material"></el-input>
+              </el-col>
+              <div class="choiceBox">
+                <i class="el-icon-document-copy"></i>
+              </div>
+              <!--物料版本-->
+              <div class="version">
                 <el-form>
-                    <el-form-item label="Kay_自定义字段1：">
-                        <el-col :span="14">
-                            <el-input v-model="kays.kays_1" ></el-input>
-                        </el-col>
-                    </el-form-item>
-                    <el-form-item label="Kay_自定义字段2：">
-                        <el-col :span="14">
-                            <el-input v-model="kays.kays_2" ></el-input>
-                        </el-col>
-                    </el-form-item>
-                    <el-form-item label="Kay_自定义字段3：">
-                        <el-col :span="14" style="margin-right:7px;">
-                            <el-input v-model="kays.kays_3" :disabled="true"></el-input>
-                        </el-col>
-                        <div class="choiceBox"><i class="el-icon-document-copy"></i></div>
-                    </el-form-item>
+                  <el-form-item label="版本：">
+                    <el-col :span="12">
+                      <el-input v-model="ruleForm.materialVer"></el-input>
+                    </el-col>
+                  </el-form-item>
                 </el-form>
-            </el-tab-pane>
-        </el-tabs>
+              </div>
+            </el-form-item>
+            <el-form-item label="计划工艺路线:" label-width="150">
+              <el-col :span="9" style="margin-right:7px;">
+                <el-input placeholder="计划工艺路线" v-model="ruleForm.technologyLine"></el-input>
+              </el-col>
+              <div class="choiceBox">
+                <i class="el-icon-document-copy"></i>
+              </div>
+              <!--计划工艺路线版本-->
+              <div class="version">
+                <el-form>
+                  <el-form-item label="版本：">
+                    <el-col :span="12">
+                      <el-input v-model="ruleForm.technologyLineVer"></el-input>
+                    </el-col>
+                  </el-form-item>
+                </el-form>
+              </div>
+            </el-form-item>
+            <el-form-item label="生产数量:" prop="number">
+              <el-col :span="9" style="margin-right:31px;">
+                <el-input placeholder="请输入生产数量" v-model="ruleForm.number"></el-input>
+              </el-col>
+              <!--已下达数量-->
+              <div>
+                <el-form>
+                  <el-form-item label="已下达数量：">
+                    <el-col :span="14">
+                      <el-input v-model="ruleForm.issuedNum" :disabled="true"></el-input>
+                    </el-col>
+                  </el-form-item>
+                </el-form>
+              </div>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane label="自定义字段">
+          <el-form>
+            <el-form-item label="Kay_自定义字段1：">
+              <el-col :span="14">
+                <el-input v-model="kays.kays_1"></el-input>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="Kay_自定义字段2：">
+              <el-col :span="14">
+                <el-input v-model="kays.kays_2"></el-input>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="Kay_自定义字段3：">
+              <el-col :span="14" style="margin-right:7px;">
+                <el-input v-model="kays.kays_3" :disabled="true"></el-input>
+              </el-col>
+              <div class="choiceBox">
+                <i class="el-icon-document-copy"></i>
+              </div>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+      </el-tabs>
     </div>
-
   </div>
 </template>
 
 <script>
+import{
+    findShopOrderRequest
+} from '@/api/work-order/work-order.api.js' 
 export default {
-    data(){
-        return{
-            workOrder:'',//工单
-            ruleForm:{
-                style:'',//类型
-                state:'',//状态
-                material:'',//物料
-                materialVer:'',//物料版本
-                technologyLine:'',//计划工艺路线
-                number:'',//生产数量
-                technologyLineVer:'',//计划工艺路线版本
-                issuedNum:'',//已下达数量
-
-            },
-            kays:{
-                kay_1:'',
-                kay_2:'',
-                kay_3:'',
-            },
-            rules: {
-                style: [
-                    { required: true, message: '请选择类型', trigger: 'blur' }
-                ],
-                state: [
-                    { required: true, message: '请选择状态', trigger: 'change' }
-                ],
-                material: [
-                    { required: true, message: '请输入计划物料', trigger: 'change' }
-                ],
-                number: [
-                    { required: true, message: '请输入数量', trigger: 'change' }
-                ],
-            }
-        }
-    }
-}
+  data() {
+    return {
+        ruleForm: {
+            style: "", //类型
+            state: "", //状态
+            material: "", //物料
+            materialVer: "", //物料版本
+            technologyLine: "", //计划工艺路线
+            number: "", //生产数量
+            technologyLineVer: "", //计划工艺路线版本
+            issuedNum: "" //已下达数量
+        },
+        kays: {
+            kay_1: "",
+            kay_2: "",
+            kay_3: ""
+        },
+        rules: {
+            style: [{ required: true, message: "请选择类型", trigger: "blur" }],
+            state: [{ required: true, message: "请选择状态", trigger: "change" }],
+            material: [
+                { required: true, message: "请输入计划物料", trigger: "change" }
+            ],
+            number: [{ required: true, message: "请输入数量", trigger: "change" }]
+        },
+        shopOrder:'',//工单号
+        tenantSiteCode:'',
+        allOrders:[],//获取到的所有工单
+        getSearchData:'',//查询获取的工单数据
+    };
+  },
+  methods:{
+      //查询工单
+      getOrder(){
+          const params ={
+              shopOrder:this.shopOrder,
+              tenantSiteCode:this.tenantSiteCode
+          }
+          findShopOrderRequest(params).then(data =>{
+              const res = data.data
+            //   console.log('获取工单'+JSON.stringify(data))
+              if(res.code == 200){
+                  this.getSearchData = res.data.shopOrder
+                  console.log('获取工单所有信息'+JSON.stringify(res.data.shopOrder))
+              }
+          })
+      }
+  },
+  created(){
+  }
+};
 </script>
 
 <style lang="scss">
@@ -178,7 +195,7 @@ export default {
     justify-content: space-between;
     .left {
       width: 300px;
-      margin-right:7px;
+      margin-right: 7px;
     }
 
     .right {
@@ -191,19 +208,19 @@ export default {
   }
 }
 //选择弹框
-.choiceBox{
-    width:15px;
-    height:41px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right:50px;
+.choiceBox {
+  width: 15px;
+  height: 41px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 50px;
 }
-.version{
-    width:300px;
-    display: inline-block
+.version {
+  width: 300px;
+  display: inline-block;
 }
-.el-form-item__content{
-    display: flex;
+.el-form-item__content {
+  display: flex;
 }
 </style>
