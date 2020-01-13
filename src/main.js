a1a0d8ba366c3c78bbbd150ec13e0f16c4fd2340
刘小斌 <liuxiaobin@hopexfintech.com>
2020-01-13 16:27:34 +0800

    注册vue-drag-resize 组件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './config'
 import ElementUI from 'element-ui';
+import VueDraggableResizable from 'vue-draggable-resizable'
+
+// optionally import default styles
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
+
 import './assets/css/element-variables.scss';
 import './assets/css/base.css'
 import '@/components'
@@ -9,6 +14,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 Vue.use(ElementUI);
+Vue.component('vue-draggable-resizable', VueDraggableResizable)
 Vue.config.productionTip = false
 console.log(process.env)
 new Vue({
