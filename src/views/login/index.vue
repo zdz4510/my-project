49438a03a91e50ab10b1623bb250934f3bcf502b
lxb <xiaobin.liu@percent.cn>
2020-02-03 09:41:47 +0800

    登陆成功跳转到首页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -66,7 +66,7 @@ export default {
               data.tokenId = tokenId;
               this.$store.dispatch("setInfo", data);
               this.$router.push({
-                path: "/base/role"
+                path: "/"
               });
             } else {
               this.$message({
