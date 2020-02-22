fc9af11deda434b0318311d6d286de11459b0f63
lxb <xiaobin.liu@percent.cn>
2020-02-22 15:48:15 +0800

    树形选中table

---
+++ src/components/global/Dialog/DsnHelpFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="help-footer">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss">
+.help-footer{
+    padding: 10px;
+    border: 1px solid #eee;
+    height: 150px;
+    overflow-y: scroll;
+    .help-item{
+        margin-bottom: 8px;
+    }
+}
+</style>
---
+++ src/components/global/Dialog/DsnHelpItem.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="help-item">
+    <div class="dsn-router-history-box">
+      <span class="left_com" >
+          <slot></slot>
+      </span>
+      <span class="right_com"
+        ><i class="el-icon-close" @click.self="close"></i
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "helpItem",
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    close() {
+        console.log('close')
+        this.$emit("close", this.item)
+     
+    },
+    toPage(item) {
+      if (this.$route.name === item.name) {
+        return;
+      }
+
+      this.$emit("toPage", item);
+    }
+  }
+};
+</script>
+<style lang="scss">
+.help-item {
+  background: #fff;
+  border-radius: 3px;
+  margin-right: 6px;
+  color: #808695;
+  border: 1px solid #eee;
+    height: 26px;
+    border-radius: 20px;
+  display: inline-block;
+  // border: 1px solid #e4e7ed;
+  box-sizing: border-box;
+  &.active {
+    color: #2173dc;
+    .left_com {
+      color: #2173dc !important;
+    }
+  }
+  .dsn-router-history-box {
+    position: relative;
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px 10px;
+    cursor: pointer;
+    .left_com {
+      flex: 1;
+      text-align: center;
+      font-size: 12px;
+      line-height: 1.5;
+      margin-right: 5px;
+      color: #808695;
+    }
+    .right_com {
+      width: 22px;
+      transform: translateZ(0);
+      margin-right: -6px;
+      font-size: 12px;
+      color: #999;
+      text-align: center;
+    }
+  }
+  .dsn-router-history-box i.el-icon-close {
+    opacity: 1;
+    // position: absolute
+  }
+  // .dsn-router-history-box:hover {
+  //   transition: all 0.3s ease-in-out;
+  //   padding-left: 10px;
+  //   i {
+  //     opacity: 1;
+  //   }
+  // }
+}
+</style>
